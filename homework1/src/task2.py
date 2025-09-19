511e89e65808715f33524f8978b34449912f6666
Lucas Estevez <lucasestevez2004@gmail.com>
2025-09-19 16:14:07 -0600

    started task2

---
+++ homework1/src/task2.py
@@ -0,0 +1,14 @@
+def exponent(int1,int2):
+    int1 = int(int1)
+    int2 = int(int2)
+
+    return int1**int2
+
+def concat(str1,str2):
+    return str1+str2
+
+def mod(flt1,flt2):
+    return flt1%flt2
+
+
+
