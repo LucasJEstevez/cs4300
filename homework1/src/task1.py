dbf1eecbdd313dfb6ebe2d2947f20408cc678a12
Lucas Estevez <lucasestevez2004@gmail.com>
2025-09-19 15:36:05 -0600

    finished task 1

--- homework1/src/task1.py
+++ homework1/src/task1.py
@@ -1,4 +1,7 @@
+#I'm not exactly sure which one I was supposed to do, here's both
+
 def f():
     return("Hello world!")
 
-print(f())
+def g():
+    print("Hello world!")
--- homework1/tests/test_task1.py
+++ homework1/tests/test_task1.py
@@ -1,5 +1,13 @@
-from src.task1 import f
+from src.task1 import f,g
 import pytest
 
+#for a basic return stmt
 def test_task_1():
     assert f() == "Hello world!"
+
+#capture stdout for a print stmt
+def test_task_2(capfd):
+    g()
+    out,err = capfd.readouterr()
+    assert out=="Hello world!\n"
+
