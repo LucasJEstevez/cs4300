92173b566c452205e00da592b71ace579967f813
Lucas Estevez <lucasestevez2004@gmail.com>
2025-09-23 03:12:36 +0000

    task 5 dictionary functional

---
+++ homework1/src/task5.py
@@ -0,0 +1,19 @@
+
+books = [
+    ['The Hunger Games','Suzanne Collins'],
+    ['Charlie and the Chocolate Factory','Roald Dahl'],
+    ['Summer of \'49','David Halberstam'],
+    ['Catcher in the Wry','Bob Uecker']]
+
+studentDB={
+    'Lucas Estevez':42,
+    'Jackson Seales':420,
+    'Michal Lustig':505,
+    'Daniel Barbotko':69
+}
+
+#def task5_books():
+#    return(books[0:3])
+
+def task5_getID(student):
+    return(studentDB[student])
---
+++ homework1/tests/test_task5.py
@@ -0,0 +1,9 @@
+import pytest
+from src.task5 import task5_getID#,task5_books
+
+#def test_task5_books():
+#    assert task5_books() == str([['The Hunger Games', 'Suzanne Collins'], ['Charlie and the Chocolate Factory', 'Roald Dahl'], ["Summer of '49", 'David Halberstam']])
+
+def test_task5_getID():
+    assert task5_getID('Lucas Estevez') == 42
+    assert task5_getID('Daniel Barbotko') == 69
