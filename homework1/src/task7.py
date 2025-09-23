8a3324768e2287a950c4d2e6cc12168aa465c4d1
Lucas Estevez <lucasestevez2004@gmail.com>
2025-09-23 16:14:29 +0000

    tasks 6/7, haha

---
+++ homework1/src/task7.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def task7_array_add(arr1,arr2):
+    return np.add(arr1,arr2)
---
+++ homework1/tests/test_task6.py
@@ -0,0 +1,5 @@
+import pytest
+from src.task6 import task6_read_file
+
+def test_task6_read_file():
+    assert task6_read_file() == 104
